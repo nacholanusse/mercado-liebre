a9c1edc9661d0871720f2af8d71ed030ae046c94
Ignacio <ignaciolanusse1@gmail.com>
2023-01-17 19:51:56 -0300

    subida del proyecto

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log('server listening on port 3000')
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
